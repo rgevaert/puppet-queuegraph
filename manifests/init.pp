00cfb089ea9a34b466777a2bc581eb2929a91784
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-04-03 15:14:39 +0200

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# Class: queuegraph
+#
+# This module manages queuegraph
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class queuegraph {
+
+  package {
+    'queuegraph':
+      ensure => installed;
+  }
+
+}
